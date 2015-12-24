aaa1de6ad51a34e9f7294bc084570742a1ff0605
kariryaa <kariryaa@cse.iitk.ac.in>
2015-12-24 21:51:30 +0530

    Let the game begin.3

---
+++ player.py
@@ -0,0 +1,24 @@
+
+initialMoney = 1000
+
+class Player():
+    def __init__(self,id,name="John"):
+        self.id = id
+        self.name = name
+        self.reset()
+
+    def reset(self):
+        self.fold = False
+        self.turn = -1
+        self.money = initialMoney
+        self.pot = 0
+
+    def bet(self,amount):
+        self.money -= amount
+        self.pot += amount
+
+    def fold(self):
+        self.fold = True
+
+if __name__ == '__main__':
+    obj = Player(0)
--- serverGame.py
+++ serverGame.py
@@ -1,14 +1,64 @@
 from deck import *
+from player import *
 
 deck = Deck()
+cards = {0:[]}
+players = {0:[]}
+pot = 0
+tableCards = []
+turn = -1
+smallBlind = 10
+bigBlind = smallBlind * 2
+start = 0
+lastRaisedPlayer = -1
+serverTurn = 0
 
 def init(clients):
     numberOfPlayers = len(clients)
-    #Initializing cards
+    #Initializing cards and players
     for i in range(0, numberOfPlayers):
-        cards[i]=(deck.pop(),deck.pop())
+        cards[i] = (deck.pop(),deck.pop())
+        players[i] = Player(i)
 
+    serverTurn = 0
+    players.get(start).bet(smallBlind)
+    players.get((start+1)%numberOfPlayers).bet(bigBlind)
+    turn = (start + 2)%numberOfPlayers
+    pot = smallBlind + bigBlind
+    lastRaisedPlayer = turn
+    #wake_clients(clients)
 
+#what happens if player folds?
+#suggestions:maintain new numberOfPlayers, players, turns etc for each round
+#one match = several games, one game = 4 rounds
+def one_round():
+    while True:
+        wait_for_client()
+        update_game()
 
-def main(clients):
+        turn = (turn+1)%numberOfPlayers
+        if turn == lastRaisedPlayer:
+            break
+
+def start_game():
+    init_broadcast()
+
+    one_round()
+    tableCards.append(deck.pop())
+    tableCards.append(deck.pop())
+    tableCards.append(deck.pop())
+    one_round()
+    tableCards.append(deck.pop())
+    one_round()
+    tableCards.append(deck.pop())
+
+    result()
+
+def main():#clients):
+    clients = [1,2,3]
     init(clients)
+    start_game()
+
+
+if __name__ == '__main__':
+    main()
